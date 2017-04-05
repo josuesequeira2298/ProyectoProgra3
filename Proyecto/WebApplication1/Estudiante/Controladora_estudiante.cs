12ccd6c2a6d6d6839bf7fff948690c6023472c7c
josuesequeira2298 <josue_2298_@outlook.com>
2017-04-04 21:14:40 -0600

    commit

--- Proyecto/WebApplication1/Estudiante/Controladora_estudiante.cs
+++ Proyecto/WebApplication1/Estudiante/Controladora_estudiante.cs
@@ -7,5 +7,12 @@ namespace WebApplication1.Estudiante
 {
     public class Controladora_estudiante
     {
+        ControladoraBD_estudiante controladoraBD = new ControladoraBD_estudiante();
+
+        internal void insertarDatos(object[] objeto)
+        {
+            Entidad_estudiante estudiante = new Entidad_estudiante();
+            
+        }
     }
 }
--- Proyecto/WebApplication1/Estudiante/Estudiante.aspx.cs
+++ Proyecto/WebApplication1/Estudiante/Estudiante.aspx.cs
@@ -9,9 +9,24 @@ namespace WebApplication1.Estudiante
 {
     public partial class Estudiante : System.Web.UI.Page
     {
+        Controladora_estudiante controladora = new Controladora_estudiante();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void datosEstudiante(object sender, EventArgs e)
+        {
+            Object[] objeto = new Object[4];
+            objeto[0] = txtNombre.Text;
+            objeto[1] = txtApellido1.Text;
+            objeto[2] = txtApellido2.Text;
+            objeto[3] = txtGenero.Text;
+            objeto[4] = txtCarnet.Text;
+            objeto[4] = txtTelefono.Text;
+            objeto[4] = txtCorreo.Text;
+            objeto[4] = txtDireccion.Text;
+
+            controladora.insertarDatos(objeto);
         }
     }
 }
