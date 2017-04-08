49b2c30a62d9b4f0dbacc8a28e65d8953f04606d
jijb95 <jorgeisaacjimenezb@hotmail.com>
2017-04-08 01:04:01 -0600

    controladora bd estudiante

--- Proyecto/WebApplication1/Estudiante/ControladoraBD_estudiante.cs
+++ Proyecto/WebApplication1/Estudiante/ControladoraBD_estudiante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -12,7 +13,28 @@ namespace WebApplication1.Estudiante
         internal void agregarEstudiante(Entidad_estudiante estudiante)
         {
             string consulta = "";
-            adaptador.insertar(consulta);
+            SqlCommand comando = new SqlCommand(consulta);
+            SqlParameter carnet = new SqlParameter("@carnet", System.Data.SqlDbType.Int);
+            carnet.Value = estudiante.Carnet;
+            SqlParameter cedula = new SqlParameter("@cedula", System.Data.SqlDbType.Int);
+            cedula.Value = estudiante.Cedula;
+            SqlParameter genero = new SqlParameter("@genero", System.Data.SqlDbType.Bit);
+            genero.Value = estudiante.Genero;
+            SqlParameter telefono = new SqlParameter("@telefono", System.Data.SqlDbType.Int);
+            telefono.Value = estudiante.Telefono;
+            SqlParameter email = new SqlParameter("@email", System.Data.SqlDbType.VarChar, 30);
+            email.Value = estudiante.Email;
+            SqlParameter iddireccion = new SqlParameter("@iddireccion",System.Data.SqlDbType.SmallInt);
+            iddireccion.Value = estudiante.id_direc;
+            SqlParameter activo = new SqlParameter("@activo",System.Data.SqlDbType.Bit);
+            activo.Value = estudiante.Activo;
+            SqlParameter nombre = new SqlParameter("@nombre", System.Data.SqlDbType.VarChar, 20);
+            nombre.Value = estudiante.Nombre;
+            SqlParameter apellido = new SqlParameter("@apellido", System.Data.SqlDbType.VarChar, 15);
+            apellido.Value = estudiante.Apellido;
+            SqlParameter apellido2 = new SqlParameter("@apellido2", System.Data.SqlDbType.VarChar, 15);
+            apellido2.Value = estudiante.Apellido2;
+
         }
 
         internal void buscarEstudiante(Entidad_estudiante estudiante)
