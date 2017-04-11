680d6defc169be3657afb04858413aeef6b4497b
jijb95 <jorgeisaacjimenezb@hotmail.com>
2017-04-11 12:58:55 -0600

    falta edit perfil

---
+++ Proyecto/WebApplication1/Perfil/ControladoraBD horario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Perfil
+{
+    public class ControladoraBD_horario
+    {
+        Adaptador adaptador = new Adaptador();
+        DataTable dt = new DataTable();
+
+        internal DataTable cargatHorario(string user)
+        {
+            string consulta = "SELECT ex.[id_curso] as Codigo,cu.nombre as Curso, gr.grupo as Grupo, gr.turno as Turno FROM [dbo].[Expediente] ex join grupo gr on gr.id_curso= ex.Id_curso join curso cu on cu.id_curso= ex.id_curso  where estado='Matriculado' and carnet= " + user;
+           dt= adaptador.consultar(consulta);
+            return dt;
+        }
+    }
+}
--- Proyecto/WebApplication1/Perfil/horario.aspx.cs
+++ Proyecto/WebApplication1/Perfil/horario.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,12 +10,20 @@ namespace WebApplication1.Perfil
 {
     public partial class horario : System.Web.UI.Page
     {
+        DataTable dt = new DataTable();
+        Controladora_Horario controladora = new Controladora_Horario();
         protected void Page_Load(object sender, EventArgs e)
         {
+
             if (Session["user"] == null)
             {
                 Response.Redirect("~/Index.aspx");
             }
+            string user = Session["user"].ToString();
+            dt = controladora.buscarHorario(user);
+            dgvhorario.DataSource = dt;
+            DataBind();
         }
+
     }
 }
